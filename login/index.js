81ce80bcfe0d27eb4db5f5ca82628bce90dc7214
unknown <candeviolam@gmail.com>
2023-03-29 20:50:23 -0300

    ejemplo de login, clase 12 (local storage)

---
+++ login/index.js
@@ -0,0 +1,74 @@
+//#region Login
+const MENSAJES = {
+  error: "Las contraseñas deben coincidir",
+};
+
+//recuperar los elems del html a los cuales les vamos a asignar los eventos que deseamos escuchar
+const submit = document.getElementById("submit");
+const inputMail = document.getElementById("inputmail");
+const inputPassword = document.getElementById("inputpass");
+
+let mail = "";
+let password = "";
+
+//                               función anónima
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  const usuariolocalStorage = localStorage.getitem(JSON.parse("usuario"));
+});
+
+inputMail.addEventListener("change", function (e) {
+  //     para que me lleve al valor que ingresó el usuario
+  mail = e.target.value;
+});
+
+inputPassword.addEventListener("change", function (e) {
+  //     para que me lleve al valor que ingresó el usuario
+  password = e.target.value;
+});
+//#endregion
+
+//#region Creación usuario
+const inputCreateMail = document.getElementById("inputcreatemail");
+const inputCreatePass = document.getElementById("inputcreatepass");
+const inputRepeatPass = document.getElementById("inputrepeatpass");
+const lblError = document.querySelector("#lblError");
+const saveChanges = document.getElementById("btnsave");
+
+let createMail = "";
+let createPass = "";
+let repeatPass = "";
+
+inputCreateMail.addEventListener("change", function (e) {
+  createMail = e.target.value;
+});
+
+inputCreatePass.addEventListener("change", function (e) {
+  createPass = e.target.value;
+});
+
+inputRepeatPass.addEventListener("change", function (e) {
+  repeatPass = e.target.value;
+});
+
+saveChanges.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (createPass !== repeatPass) {
+    lblError.innerHTML = MENSAJES.error;
+    lblError.style.display = "block";
+    setTimeout(() => {
+      lblError.style.display = "none";
+    }, 1000);
+  } else {
+    localStorage.setItem(
+      "usuario",
+      JSON.stringify({
+        nombre: createMail,
+        password: createPass,
+      })
+    );
+  }
+});
+
+//#endregion
